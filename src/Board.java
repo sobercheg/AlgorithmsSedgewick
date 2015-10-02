e5aac8f6fa3499b6b79f0c1bea99afe71de22c27
Andrei Puhach <sobercheg@gmail.com>
2015-10-02 08:51:26 -0700

    Assignment 4: 8 puzzle. Solution 100%

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.Iterator;
+
+import edu.princeton.cs.algs4.Queue;
+
 /**
  * Programming Assignment 4.
  * 8 Puzzle. http://coursera.cs.princeton.edu/algs4/assignments/8puzzle.html
@@ -6,49 +11,179 @@ public class Board {
 
   // (where blocks[i][j] = block in row i, column j)
   private final int[][] blocks;
+  private final int n;
 
   public Board(int[][] blocks) {
-    this.blocks = blocks;
+    this.blocks = new int[blocks.length][blocks[0].length];
+    for (int i = 0; i < blocks.length; i++) {
+      assert blocks.length == blocks[i].length : "Board should be a square";
+      for (int j = 0; j < blocks[i].length; j++) {
+        this.blocks[i][j] = blocks[i][j];
+      }
+    }
+    this.n = blocks.length;
   }
 
   public int dimension() {
-    return blocks.length;
+    return n;
   }
 
   /**
-   * Number of blocks out of place
+   * Number of blocks out of place Hamming priority function. The number of blocks in the wrong
+   * position, plus the number of moves made so far to get to the search node. Intuitively, a search
+   * node with a small number of blocks in the wrong position is close to the goal, and we prefer a
+   * search node that have been reached using a small number of moves.
    */
   public int hamming() {
-    return 0;
+    int wrongBlocks = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (blocks[i][j] > 0 && blocks[i][j] != i * n + j + 1) {
+          wrongBlocks++;
+        }
+      }
+    }
+    return wrongBlocks;
   }
 
   /**
-   * Sum of Manhattan distances between blocks and goal
+   * Sum of Manhattan distances between blocks and goal. The sum of the Manhattan distances (sum of
+   * the vertical and horizontal distance) from the blocks to their goal positions, plus the number
+   * of moves made so far to get to the search node.
    */
   public int manhattan() {
-    return 0;
+    int wrongBlocks = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (blocks[i][j] == 0) {
+          continue;
+        }
+        int[] desiredPosition = getPosition(blocks[i][j]);
+        wrongBlocks += Math.abs(i - desiredPosition[0]) + Math.abs(j - desiredPosition[1]);
+      }
+    }
+    return wrongBlocks;
+  }
+
+  private int[] getPosition(int value) {
+    if (value == 0) {
+      return new int[]{n - 1, n - 1};
+    }
+    return new int[]{(value - 1) / n, (value - 1) % n};
   }
 
   public boolean isGoal() {
-    return false;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        int[] desiredPosition = getPosition(blocks[i][j]);
+        if (desiredPosition[0] != i || desiredPosition[1] != j) {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 
   /**
-   * A board that is obtained by exchanging any pair of blocks
+   * A board that is obtained by exchanging any pair of blocks.
    */
   public Board twin() {
-    return null;
+    int[][] twin = getClone();
+
+    if (twin[0][0] > 0 && twin[0][1] > 0) {
+      swap(twin, 0, 0, 0, 1);
+    } else if (twin[0][0] > 0 && twin[1][0] > 0) {
+      swap(twin, 0, 0, 1, 0);
+    } else if (twin[0][1] > 0 && twin[1][1] > 0) {
+      swap(twin, 0, 1, 1, 1);
+    } else if (twin[1][0] > 0 && twin[1][1] > 0) {
+      swap(twin, 1, 0, 1, 1);
+    } else {
+      throw new IllegalStateException(
+          "Cannot exchange a pair of blocks. That should never happen! Debug!");
+    }
+    return new Board(twin);
   }
 
+  private int[][] getClone() {
+    int[][] clone = new int[n][n];
+    for (int i = 0; i < blocks.length; i++) {
+      for (int j = 0; j < blocks[i].length; j++) {
+        clone[i][j] = blocks[i][j];
+      }
+    }
+    return clone;
+  }
+
+  private void swap(int[][] array, int i1, int j1, int i2, int j2) {
+    int tmp = array[i1][j1];
+    array[i1][j1] = array[i2][j2];
+    array[i2][j2] = tmp;
+  }
   public boolean equals(Object y) {
-    return false;
+    if (this == y) {
+      return true;
+    }
+    if (y == null) {
+      return false;
+    }
+    if (this.getClass() != y.getClass()) {
+      return false;
+    }
+    Board that = (Board) y;
+    if (that.blocks.length != blocks.length) {
+      return false;
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (!Arrays.equals(blocks[i], that.blocks[i])) {
+        return false;
+      }
+    }
+    return true;
   }
 
   /**
-   * All neighboring boards
+   * All neighboring boards.
    */
   public Iterable<Board> neighbors() {
-    return null;
+    int zeroRow = 0;
+    int zeroCol = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (blocks[i][j] == 0) {
+          zeroRow = i;
+          zeroCol = j;
+        }
+      }
+    }
+
+    Queue<Board> neighbors = new Queue<>();
+    // Right neighbor
+    if (zeroCol < n - 1) {
+      int[][] neighbor = getClone();
+      swap(neighbor, zeroRow, zeroCol, zeroRow, zeroCol + 1);
+      neighbors.enqueue(new Board(neighbor));
+    }
+    // Left neighbor
+    if (zeroCol > 0) {
+      int[][] neighbor = getClone();
+      swap(neighbor, zeroRow, zeroCol, zeroRow, zeroCol - 1);
+      neighbors.enqueue(new Board(neighbor));
+    }
+    // Bottom neighbor
+    if (zeroRow < n - 1) {
+      int[][] neighbor = getClone();
+      swap(neighbor, zeroRow, zeroCol, zeroRow + 1, zeroCol);
+      neighbors.enqueue(new Board(neighbor));
+    }
+    // Top neighbor
+    if (zeroRow > 0) {
+      int[][] neighbor = getClone();
+      swap(neighbor, zeroRow, zeroCol, zeroRow - 1, zeroCol);
+      neighbors.enqueue(new Board(neighbor));
+    }
+    return neighbors;
   }
 
   /**
@@ -62,10 +197,62 @@ public class Board {
    * </pre>
    */
   public String toString() {
-    return "";
+    StringBuilder s = new StringBuilder();
+    s.append(n).append("\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            s.append(String.format("%2d ", blocks[i][j]));
+        }
+        s.append("\n");
+    }
+    return s.toString();
   }
 
   public static void main(String[] args) {
+    Board board = new Board(new int[][]{
+        {0, 1, 3},
+        {4, 2, 5},
+        {7, 8, 6}
+    });
+    assertEquals(board.dimension(), 3, "dimension");
+    assertEquals(board.hamming(), 4, "hamming");
+    assertEquals(board.manhattan(), 4, "manhattan");
+    assertEquals(board.isGoal(), false, "not goal");
+    assertEquals(new Board(new int[][]{
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 0}
+    }).isGoal(), true, "goal");
+    assertEquals(board.toString(), "3\n"
+        + " 0  1  3 \n"
+        + " 4  2  5 \n"
+        + " 7  8  6 \n", "toString()");
 
+    Iterator<Board> neighbors = board.neighbors().iterator();
+    assertEquals(neighbors.next(), new Board(new int[][] {
+        {1, 0, 3},
+        {4, 2, 5},
+        {7, 8, 6}
+    }), "right neighbor");
+    assertEquals(neighbors.next(), new Board(new int[][] {
+        {4, 1, 3},
+        {0, 2, 5},
+        {7, 8, 6}
+    }), "bottom neighbor");
+    assertEquals(neighbors.hasNext(), false, "no more neighbors");
+
+    assertEquals(board.twin(), new Board(new int[][] {
+        {0, 2, 3},
+        {4, 1, 5},
+        {7, 8, 6}
+    }), "twin");
+  }
+
+  private static void assertEquals(Object actual, Object expected, String msg) {
+    if (actual.equals(expected)) {
+      System.out.printf("%s: Passed. Actual: %s%n", msg, actual);
+    } else {
+      System.err.printf("%s: Failed. Actual: %s, expected: %s%n", msg, actual, expected);
+    }
   }
 }
--- src/Solver.java
+++ src/Solver.java
@@ -1,38 +1,148 @@
+import java.util.Comparator;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
 /**
- * Programming Assignment 4. 8 puzzle.
- * http://coursera.cs.princeton.edu/algs4/assignments/8puzzle.html
+ * Programming Assignment 4.
+ * 8 puzzle. http://coursera.cs.princeton.edu/algs4/assignments/8puzzle.html
  */
 public class Solver {
+
+  private boolean isSolvable;
+  private SearchNode solutionNode;
 
   /**
    * Find a solution to the initial board (using the A* algorithm)
    */
   public Solver(Board initial) {
+    if (initial == null) {
+      throw new NullPointerException("Initial board cannot be null");
+    }
+    MinPQ<SearchNode> q = new MinPQ<>(new SearchNodeComparator());
+    MinPQ<SearchNode> qTwin = new MinPQ<>(new SearchNodeComparator());
+    q.insert(new SearchNode(initial, null, 0));
+    qTwin.insert(new SearchNode(initial.twin(), null, 0));
 
+    do {
+      SearchNode node = q.delMin();
+      SearchNode twinNode = qTwin.delMin();
+      if (node.board.isGoal()) {
+        solutionNode = node;
+        isSolvable = true;
+        break;
+      }
+      if (twinNode.board.isGoal()) {
+        isSolvable = false;
+        break;
+      }
+      SearchNode previous = node.previous;
+      for (Board neighbor : node.board.neighbors()) {
+        if (previous == null || !neighbor.equals(previous.board)) {
+          q.insert(new SearchNode(neighbor, node, node.moves + 1));
+        }
+      }
+      SearchNode previousTwin = twinNode.previous;
+      for (Board neighbor : twinNode.board.neighbors()) {
+        if (previousTwin == null || !neighbor.equals(previousTwin.board)) {
+          qTwin.insert(new SearchNode(neighbor, twinNode, twinNode.moves + 1));
+        }
+      }
+    } while (true);
+  }
+
+  private class SearchNodeComparator implements Comparator<SearchNode> {
+
+    @Override
+    public int compare(SearchNode o1, SearchNode o2) {
+      return Integer.compare(o1.board.manhattan() + o1.moves, o2.board.manhattan() + o2.moves);
+    }
+  }
+
+  private class SearchNode implements Comparable<SearchNode> {
+
+    private Board board;
+    private SearchNode previous;
+    private int moves;
+
+    SearchNode(Board board, SearchNode previous, int moves) {
+      this.board = board;
+      this.previous = previous;
+      this.moves = moves;
+    }
+
+    @Override
+    public int compareTo(SearchNode o) {
+      return Integer.compare(board.manhattan() + moves, o.board.manhattan() + o.moves);
+    }
   }
 
   public boolean isSolvable() {
-    return false;
+    return isSolvable;
   }
 
   /**
    * Min number of moves to solve initial board; -1 if unsolvable
    */
   public int moves() {
-    return 0;
+    if (solutionNode != null) {
+      return solutionNode.moves;
+    }
+    return -1;
   }
 
   /**
    * Sequence of boards in a shortest solution; null if unsolvable
    */
   public Iterable<Board> solution() {
-    return null;
+    if (solutionNode == null) {
+      return null;
+    }
+    Stack<Board> solution = new Stack<>();
+    SearchNode node = solutionNode;
+    while (node != null) {
+      solution.push(node.board);
+      node = node.previous;
+    }
+    return solution;
   }
 
   /**
    * Solve a slider puzzle (given below)
    */
   public static void main(String[] args) {
+    // for each command-line argument
+    for (String filename : args) {
 
+      // read in the board specified in the filename
+      In in = new In(filename);
+      int n = in.readInt();
+      int[][] tiles = new int[n][n];
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          tiles[i][j] = in.readInt();
+        }
+      }
+
+      // solve the slider puzzle
+      Board initial = new Board(tiles);
+      Solver solver = new Solver(initial);
+      StdOut.println(filename);
+      StdOut.println("Minimum number of moves = " + solver.moves());
+      StdOut.println();
+      for (Board board : solver.solution()) {
+        StdOut.println(board);
+      }
+    }
+  }
+
+  private static void assertEquals(Object actual, Object expected, String msg) {
+    if (actual.equals(expected)) {
+      System.out.printf("%s: Passed. Actual: %s%n", msg, actual);
+    } else {
+      System.err.printf("%s: Failed. Actual: %s, expected: %s%n", msg, actual, expected);
+    }
   }
 }
